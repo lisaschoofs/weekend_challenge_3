f4c992b53704f903c7648796be142b297c2a9e88
Lisa Schoofs <lisaschoofs@gmail.com>
2017-03-24 17:21:14 -0500

    sets up base task route on server side

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = 5000;
+var tasks = require('./routes/task.js');
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/tasks', tasks);
 
 app.listen(port, function(){
   console.log("listening on port", port);
